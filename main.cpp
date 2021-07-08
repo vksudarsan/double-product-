ef64ecb209627112370b39c03255fae175cde89d
vksudarsan <83401917+vksudarsan@users.noreply.github.com>
2021-07-08 19:01:15 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    cout<<"\nenter the number 1:";
+    cin>>a;
+    cout<<"\nenter the number 2:";
+    cin>>b;
+    c=a*b;
+
+    cout << "\nthe product of the numbers are:" <<c<< endl;
+    return 0;
+}
